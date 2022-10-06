43aa81df20234bf06ae33fee07348685767893bd
kaskasmano667 <pikachu_carlos@hotmail.com>
2022-10-05 21:00:48 -0500

    lol
    lol2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
@@ -9,6 +10,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule
+  ],
+  exports:[
+    SidebarComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
